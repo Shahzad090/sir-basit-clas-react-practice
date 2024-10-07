8f9f178a4faf9cfbad263e12470705fa2a92b239
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-10-07 20:54:53 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ABButton from "./component/ABButton"
+import input from "./component/input";
+
 
 function App() {
+  const buttonClick = () => {
+    console.log("Button click from parent");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save t.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <ABButton btnClick={buttonClick}
+      label="Register" className="abc" />
+      <ABButton label="" />
+      <ABButton label="login" />
+      <ABButton label="signup" />
+      <ABButton label="ok" />
+      <ABButton label="cancel" />
+      </div>
   );
 }
+
+
+<div>
+ <input palaceholder="enter name"
+</div>
 
 export default App;
