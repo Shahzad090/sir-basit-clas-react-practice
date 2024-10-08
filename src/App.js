17ba55923922ff7d9324ef5245966ab5adaf5f75
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-10-08 23:33:29 +0500

    mn

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <ABButton btnClick={buttonClick}
       label="Register" className="abc" />
       <ABButton label="" />
-      <ABButton label="l" />
+      <ABButton label="lo" />
       <ABButton label="signup" />
       <ABButton label="ok" />
       <ABButton label="cancel" />
